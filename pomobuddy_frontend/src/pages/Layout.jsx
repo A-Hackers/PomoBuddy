c1c0584261bb6c0814b1ce018e3eb3cbcd2dd281
WinterCherri <decaturhoangyen@gmail.com>
2024-05-17 00:05:43 -0400

    tasks feature no styling

--- pomobuddy_frontend/src/pages/Layout.jsx
+++ pomobuddy_frontend/src/pages/Layout.jsx
@@ -1,10 +1,12 @@
 import Timer from "../components/Timer";
+import TodoList from "../components/TodoList";
 
 const Layout = () => {
   return (
     <div>
-      <div className="bg-black">
+      <div className="bg-white">
         <Timer />
+        <TodoList />
       </div>
       <div>apples</div>
     </div>
