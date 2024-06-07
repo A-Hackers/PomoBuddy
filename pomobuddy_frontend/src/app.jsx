c8b965b21c6e43ebeb81fd961cac55a009ac6289
Sprihanjay <banik.sprihanjay@gmail.com>
2024-06-07 06:45:29 -0400

    Removed unnecessary files

--- pomobuddy_frontend/src/app.jsx
+++ pomobuddy_frontend/src/app.jsx
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import "./index.css";
-import TimerPage from "./pages/TimerPage.jsx";
-
-const App = () => {
-  return (
-    <div>
-      <TimerPage />
-    </div>
-=======
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx';
@@ -22,7 +12,6 @@ const App = () => {
       <Route path="/timer" element={<TimerPage />} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
->>>>>>> Stashed changes
   );
 };
 
